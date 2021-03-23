4a9af1170eb0876d40ecafd70a426b19bb13469c
VictorKmin <kminvictor@gmail.com>
2021-03-23 21:45:18 +0200

    Initial commit

---
+++ basic/lesson1/lesson1.js
@@ -0,0 +1,120 @@
+// // alert('HELLO');
+//
+// const name = 'Viktor';
+// let age = 25.2;
+// let girl = true;
+//
+// age = 'Twenty five';
+//
+// // alert(age);
+// // alert(name);
+//
+// // console.log(girl)
+// // console.log(25)
+// // console.log(age)
+// // console.log(name)
+//
+// // document.write(name);
+// // document.write('<br>');
+// // document.write('<br>');
+// // document.write('<br>');
+// // document.write('<br>');
+// // document.write('<br>');
+// // document.write('<br>');
+// // document.write(name);
+//
+// // const userAge = prompt('How old are u?');
+// // console.log(userAge);
+// //
+// // const isUserLikeMeat = confirm('Do you like meat?');
+// // console.log(isUserLikeMeat);
+//
+// // document.write(2 + 2);
+// // document.write(10 / 2);
+// // document.write(true + 2);
+// // document.write(true + true);
+// // document.write('2' + '2');
+// // document.write('2' - '2');
+// // document.write('2' * '2');
+// // document.write('2' / '2');
+// // document.write(10 * '5');
+// // document.write(10 * 'test');
+// // document.write(10 * true);
+// // document.write(10 / 0);
+// alert((2 + 2) * 3);
+// // console.log(16 % 3);
+//
+// console.log(typeof 2);
+// console.log(typeof 'HELLO');
+// console.log(typeof false);
+// console.log(typeof NaN);
+// console.log(typeof Infinity);
+//
+
+// < > <= >= == != === !==
+
+// console.log(2 > 4);
+// console.log(10 >= 2);
+// console.log(10 !== 2);
+// console.log(10 === 2);
+
+// console.log('2' == 2);
+// console.log('2' === 2);
+// console.log(1 == true);
+
+
+// const userName = prompt('enter name');
+// const userAge = prompt('enter age');
+//
+// if (userName === 'admin') {
+//   alert('Hello admin');
+//   alert('Welcome');
+// } else {
+//   if (userAge >= 15) {
+//     alert('Hello admin');
+//     alert('Welcome');
+//   } else {
+//     alert('Hello user');
+//     alert('Glad to see u');
+//   }
+// }
+//
+// if (userName === 'admin') {
+//   alert('Hello admin');
+//   alert('Welcome');
+// } else if (userAge >= 15) {
+//   alert('Hello admin');
+//   alert('Welcome');
+// } else {
+//   alert('Hello user');
+//   alert('Glad to see u');
+// }
+//
+// || - OR - АБО
+// && - AND - І
+// if (userName === 'admin' && userAge >= 15) {
+//   alert('Hello admin');
+//   alert('Welcome');
+// } else {
+//   alert('Hello user');
+//   alert('Glad to see u');
+// }
+
+
+
+const userName = prompt('enter name');
+
+switch (userName) {
+  case 'admin':
+    alert('Hello admin');
+    break;
+  case 'Karina':
+    alert('Meow');
+    alert('Hello baby');
+    break;
+  case 'Dima':
+    alert('Hello man');
+    break;
+  default:
+    alert("I don't know you");
+}
